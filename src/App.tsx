ba5d44de2bde18c083795d5c8e9b382dfe7d5ec6
Kristen De La Rosa <delarosa.kristen@gmail.com>
2025-02-05 14:48:53 -0800

    fix: removed protected routes for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ import { LikedDogsProvider } from "./context/LikedDogsContext";
 import { Favorites } from "./pages/Favorites/Favorites.page";
 import { Login } from "./pages/Login/Login.page";
 import { Search } from "./pages/Search/Search.page";
-// import { theme } from "./theme";
+
 import { Match } from "./pages/Match/Match.page";
-import ProtectedRoute from "./components/ProtectedRoutes/ProtectedRoutes.component";
 
 function App() {
   return (
@@ -19,11 +18,9 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Login />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="/search" element={<Search />} />
-                <Route path="/favorites" element={<Favorites />} />
-                <Route path="/match" element={<Match />} />
-              </Route>
+              <Route path="/search" element={<Search />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/match" element={<Match />} />
             </Routes>
           </BrowserRouter>
         </LikedDogsProvider>
